da365de5b5e1f477b3cdb569099705903d41b619
bharat joshi <bharatjoshi.p@gmail.com>
2018-11-01 13:17:57 +1100

    feat: add js indentation

--- config.el
+++ config.el
@@ -41,5 +41,17 @@
 
     (:after ivy
       :n "M-f" #'+ivy/project-search
+      :n "M-F" #'swiper
       )
 )
+
+(setq c-basic-offset 2)
+;; web development
+(setq coffee-tab-width 2) ; coffeescript
+(setq javascript-indent-level 2) ; javascript-mode
+(setq js-indent-level 2) ; js-mode
+(setq js2-basic-offset 2) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+(setq web-mode-markup-indent-offset 2) ; web-mode, html tag in html file
+(setq web-mode-css-indent-offset 2) ; web-mode, css in html file
+(setq web-mode-code-indent-offset 2) ; web-mode, js code in html file
+(setq css-indent-offset 2) ; css-mode
