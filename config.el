2a7bc2b5a89648e1961a009d9bd40ad61ead8001
bharat joshi <bharatjoshi.p@gmail.com>
2018-12-28 14:27:59 +1100

    fix: use super instead of Meta key. add comments for each section

--- config.el
+++ config.el
@@ -5,22 +5,25 @@
            '(prog-mode-hook text-mode-hook ))
     (add-hook hook myMode)))
 
-
+;; doom theme config
 (setq doom-theme 'doom-one)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; doom font config
 (setq doom-font (font-spec :family "Operator Mono" :size 14)
       doom-variable-pitch-font (font-spec :family "Fira Sans")
       doom-unicode-font (font-spec :family "DejaVu Sans Mono")
       doom-big-font (font-spec :family "Fira Code Mod" :size 19))
 
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
+;; enable drag-stuff-mode
 (def-package! drag-stuff
   :config
   (setq drag-stuff-mode t))
 
 (def-package! vmd-mode)
 
+;; setup highlight-indent-guides
 (def-package! highlight-indent-guides
   :config
   (setq highlight-indent-guides-method 'character)
@@ -29,10 +32,10 @@
 
 (map!
  (:after evil
-   :n "M-h" #'next-buffer
-   :n "M-l" #'previous-buffer
-   :n "M-j" #'drag-stuff-down
-   :n "M-k" #'drag-stuff-up
+   :g "s-h" #'next-buffer
+   :g "s-l" #'previous-buffer
+   :g "s-j" #'drag-stuff-down
+   :g "s-k" #'drag-stuff-up
 
    (:leader
      (:desc "Dumb jump" :prefix "d"
@@ -41,9 +44,10 @@
        :desc "Quick look"        :nv "q" #'dumb-jump-quick-look)))
 
  (:after ivy
-   :n "M-F" #'+ivy/project-search
-   :n "M-f" #'swiper	))
+   :n "s-F" #'+ivy/project-search
+   :n "s-f" #'swiper	))
 
+;; indentation config
 (setq c-basic-offset 2)
 ;; web development
 (setq coffee-tab-width 2) ; coffeescript
@@ -55,13 +59,18 @@
 (setq web-mode-code-indent-offset 2) ; web-mode, js code in html file
 (setq css-indent-offset 2) ; css-mode
 
+;; frame config
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
+
+;; enable case sensitive search in evil
 (setq evil-ex-search-case 'sensitive)
 
+;; load custom fira liguatures config
 (load-file "~/.doom.d/fira.el")
 
+;; ----------------- override font faces---------------------------------------------
 (set-face-attribute 'font-lock-comment-face nil :inherit 'font-lock-comment-face :slant 'italic)
 (set-face-attribute 'font-lock-function-name-face nil :inherit 'font-lock-function-name-face :slant 'italic)
 (set-face-attribute 'font-lock-variable-name-face nil :inherit 'font-lock-variable-name-face :slant 'italic)
@@ -80,12 +89,14 @@
 
 (set-face-attribute 'nobreak-space nil :background "maroon2")
 (set-face-attribute 'nobreak-hyphen nil :background "maroon2")
+;; ----------------- override font faces---------------------------------------------
 
+;; set minor modes to prog and text mode
 (setToTextProg #'fira-code-mode)
-
 (setq whitespace-line-column 500)
 (setToTextProg #'whitespace-mode)
 
+;;---------------------------------------handle whitespace mode with company popup
 (defvar my-prev-whitespace-mode nil)
 (make-variable-buffer-local 'my-prev-whitespace-mode)
 (defun pre-popup-draw ()
@@ -104,5 +115,7 @@
 
 (advice-add 'company-pseudo-tooltip-unhide :before #'pre-popup-draw)
 (advice-add 'company-pseudo-tooltip-hide :after #'post-popup-draw)
+;;---------------------------------------handle whitespace mode with company popup
 
+;;---------------------------------------handle whitespace mode in graphql mode
 (add-hook 'graphql-mode-hook (lambda() (whitespace-mode -1)))
