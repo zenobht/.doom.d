f376acb8c1b611037dc67e495645ff94be02c338
bharat joshi <bharatjoshi.p@gmail.com>
2018-12-29 15:53:14 +1100

    feat: enable java and kotlin mode

--- packages.el
+++ packages.el
@@ -3,4 +3,4 @@
 (package! ivy-rich :disable t)
 (package! highlight-indent-guides)
 (package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))
-
+(package! kotlin-mode)
