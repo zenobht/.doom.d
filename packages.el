872bb09a6f06dd60efa15fbaa68ae27c0b7b2df6
bharat joshi <bharatjoshi.p@gmail.com>
2018-12-30 21:22:49 +1100

    refactor: revert evil-matchit fix

--- packages.el
+++ packages.el
@@ -2,5 +2,4 @@
 (package! vmd-mode)
 (package! ivy-rich :disable t)
 (package! highlight-indent-guides)
-(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))
 (package! kotlin-mode)
