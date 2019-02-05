2898057d201f687e23eb417a3eac84d20ae4e2a7
bharat joshi <bharatjoshi.p@gmail.com>
2019-02-05 20:37:59 +1100

    feat: add sort-words function

--- config.el
+++ config.el
@@ -187,3 +187,14 @@
 (add-hook 'json-mode-hook
           (lambda ()
             (setq js-indent-level 2)))
+
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+    Prefixed with negative \\[universal-argument], sorts in reverse.
+
+    The variable `sort-fold-case' determines whether alphabetic case
+    affects the sort order.
+
+    See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
